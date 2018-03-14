3bb0ede1d9441cb9e72096b6924f85eac0d727aa
Shaan Foltz <shaanr@yahoo.com>
2018-03-14 14:08:06 -0400

    initial commit

---
+++ HashTablesClass.cs
@@ -0,0 +1,22 @@
+﻿// /****************************** Module Header ******************************\
+// Module Name:  HashTablesClass.cs
+// Project:      edX_DEV204.3x
+// Copyright (c) Shaan Foltz
+//
+// Description:  
+//
+//
+// THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+// EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+// WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+// \***************************************************************************/
+using System;
+namespace edX_DEV204.x
+{
+    public class HashTablesClass
+    {
+        public HashTablesClass()
+        {
+        }
+    }
+}
